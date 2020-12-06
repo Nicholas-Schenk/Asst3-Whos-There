30035f8272af6734652d811d8e2275a33022eebe
Nicholas-Schenk <71524899+Nicholas-Schenk@users.noreply.github.com>
2020-12-05 20:04:09 -0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char ** argv){
+	int sock, valread;
+	struct sockaddr_in serv_addr;
+	char * start = "REG|12|Who's there?|";
+	char * response = "REG|14|Djikstra, who?|";
+	char* disgust = "REG|4|Ugh.|";
+	char buffer[1024] = {};
+	if((sock = socket(AF_INET, SOCK_STREAM, 0))<0){
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0){
+		printf("\nInvalid address/ not supported\n");
+		return -1;
+	}
+
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))<0){
+		printf("\n Connection failed\n");
+		return -1;
+	}
+
+	
+	read(sock, buffer, 1024);
+	printf("%s\n", buffer);
+	send(sock, start, strlen(start), 0);
+	valread = read(sock, buffer, 1024);
+	buffer[valread] = '\0';	
+	char *temp_setup = malloc(sizeof(char)*(strlen(buffer)+7));
+	strcpy(temp_setup, "REG|");
+	int the_length = strlen(buffer)-7+5;
+	int check = 0;
+	if(strlen(buffer) > 18){
+		check = 1;
+		the_length--;
+	}
+	
+	char* temp_length = malloc(sizeof(char)*5);
+	sprintf(temp_length, "%d", the_length);
+	strcat(temp_setup, temp_length);
+	strcat(temp_setup, "|");
+	char* mytemp = malloc(sizeof(char)*the_length);
+	if(check == 1){
+		memcpy(mytemp, &buffer[7], the_length-6);
+	}else{
+		memcpy(mytemp, &buffer[6], the_length-6);
+	}
+	strcat(temp_setup, mytemp);
+	strcat(temp_setup, ", who?|");
+	//printf("%s\n", temp_setup);
+	printf("%s\n", buffer);
+	response = temp_setup;
+	send(sock, response, strlen(response), 0);
+	valread = read(sock, buffer, 1024);
+	buffer[valread] = '\0';
+	printf("%s\n", buffer);
+	send(sock, disgust, strlen(disgust), 0);
+	//read(sock, buffer, 1024);
+	close(sock);
+
+	return 0;	
+
+
+}
